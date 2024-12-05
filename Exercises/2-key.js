66f51cd6cd9d5a45d50473fe5970a47b944dcca7
Diana <navbidibi@gmail.com>
2024-12-05 20:37:18 +0200

    The tests are done

--- Exercises/2-key.js
+++ Exercises/2-key.js
@@ -1,9 +1,18 @@
 'use strict';
 
-const generateKey = (length, possible) => {
-  // Generate string of random characters
-  // Use Math.random() and Math.floor()
-  // See documentation at MDN
+const random = (min, max) => {
+  if (typeof max === 'undefined') {
+    max = min;
+    min = 0;
+  }
+  return Math.floor((Math.random() * (max - min)) + min);
 };
 
+const generateKey = (length, possible) => {
+  let strng = '';
+  for (let i = 0; i < length; i++) {
+    strng += possible[random(possible.length - 1)];
+  }
+  return strng;
+};
 module.exports = { generateKey };
